ac2e24885e98d16e205c60f87fd6a852120ddc38
LoliWolf <2233284845@qq.com>
2023-04-21 17:15:34 +0800

    first commit

---
+++ sidebar_tree/src/main/java/com/ruoyi/sidebarTree/controller/ExpertsController.java
@@ -0,0 +1,10 @@
+package com.ruoyi.sidebarTree.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/experts")
+public class ExpertsController {
+    
+}
