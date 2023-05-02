58b7176e544506ff9db3ebf22961996c6bbfe23e
Coder-RSL <a13036923860@163.com>
2023-05-02 14:51:16 +0800

    first commit

--- admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -16,6 +16,6 @@ public class RuoYiApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("Start successfully!!!!!!!!!");
+        System.out.println("Start successfully!!!sadas!!!!!!");
     }
 }
